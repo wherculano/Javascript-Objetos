74a9727cba4147f5aea61d4609407a1af5251fb9
wagner <wagner.santos@accurate.com.br>
2023-02-23 17:01:27 -0300

    Criacao de objeto: Comparando com array

---
+++ aula_001_objeto_pessoa.js
@@ -0,0 +1,9 @@
+const dadosPessoais = ["nome", "Raimundo", "idade", 42]
+console.log(`${dadosPessoais[0]}: ${dadosPessoais[1]}, ${dadosPessoais[2]}${dadosPessoais[3]}`)
+
+// Objeto: Chave-valor
+const objetoPessoa = {
+    nome: "Raimundo",
+    idade: 42
+}
+console.log(objetoPessoa)
